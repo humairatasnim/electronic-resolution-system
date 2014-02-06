2ae685ee3d406ce533810f1423f201d613ba0142
Humaira Tasnim <humaira.cmu@gmail.com>
2014-02-06 16:37:23 +0300

    added validator for resolutions document

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -6,5 +6,9 @@ class Resolution < ActiveRecord::Base
   belongs_to :country
   attr_accessible :committee_id, :status_id, :user_id, :votes_against, :votes_for, :document, :title, :country_id, :num_votes_for, :num_votes_against, :num_abstentions
   has_attached_file :document
+  validates_attachment_content_type :document, :content_type => ["application/pdf",
+  	"application/msword",
+  	"application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
+  	"text/plain"]
   validates_presence_of :title, :committee_id, :country_id, :document
 end
